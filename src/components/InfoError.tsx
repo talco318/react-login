90453332c28afab32cda26cee2416b8360bd5a5f
Tal Cohen <talcohen318@gmail.com>
2023-12-25 01:44:53 +0200

    InfoError.tsx css is better now

--- src/components/InfoError.tsx
+++ src/components/InfoError.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 export const InfoErrorComponent = () => {
     return (
-        <div>
-            <h1>Please login first!</h1>
-            <a href={"/"}>login page</a>
+        <div className='center-text'>
+            <center>
+            <h1 className='error-message'>Please login first!</h1>
+                <div className='h4'> <a href={"/"}>login page</a></div>
+            </center>
         </div>
     );
 };
